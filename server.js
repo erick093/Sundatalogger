b469c95a7cbd39bcf25b22834460091900766276
erick <erick_093@hotmail.com>
2017-06-08 14:12:17 -0400

    testin mqtt server v2

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var auth = (mqtt_url.auth || ':').split(':');
 // app.use(bodyParser.json());
 // //app.use(app.router);
 //     //app.use(express.logger());
-var client = mqtt.createClient(mqtt_url.port, mqtt_url.hostname, {
+var client = mqtt.connect(mqtt_url.port, mqtt_url.hostname, {
   username: auth[0],
   password: auth[1]
 });
